8f55cf9350ee1ab4fa28b538af62ba74463436a2
Jason Sohn <tensorturtle@gmail.com>
2021-10-03 00:36:24 +0900

    simplify main loop further by taking Predictor out

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from yolox.utils import fuse_model, get_model_info, postprocess, vis
 # Classes
 HYDO_CLASSES = ['bicycle', 'bus', 'car', 'cyclist', 'motorcycle', 'pedestrian', 'truck']
 
+# YOLOX predictor
+from yolox_inference import Predictor
+
 # SORT
 from gi_sort.sort_minimal import *
 
@@ -37,86 +40,6 @@ def make_parser():
     parser.add_argument("--fuse", dest="fuse", default=False, action = "store_true", help="Fuse conv and bn for testing")
     parser.add_argument("--trt", dest="trt", default=False, action="store_true", help="Using tensorRT for testing")
     return parser
-
-
-class Predictor():
-    def __init__(self,
-            model,
-            exp,
-            cls_names=HYDO_CLASSES,
-            trt_file = None,
-            decoder = None,
-            device = 'gpu',
-            fp16 = False,
-            ):
-        self.model = model
-        self.cls_names = cls_names
-        self.decoder = decoder
-        self.num_classes = exp.num_classes
-        self.confthre = exp.test_conf
-        self.nmsthre = exp.nmsthre
-        self.test_size = exp.test_size
-        self.device = device
-        self.fp16 = fp16
-        self.preproc = ValTransform(legacy=False)
-
-        if trt_file is not None:
-            from torch2trt import TRTModule
-
-            model_trt = TRTModule()
-            model_trt.load_state_dict(torch.load(trt_file))
-            x = torch.ones(1,3, exp.test_size[0], exp.test_size[1]).cuda()
-
-            self.model(x)
-            self.model = model_trt
-    def inference(self, img):
-        img_info = {"id":0}
-        if isinstance(img, str):
-            img_info["file_name"] = os.path.basename(img)
-            img = cv2.imread(img)
-        else:
-            img_info["file_name"] = None
-        height, width = img.shape[:2]
-        img_info["height"] = height
-        img_info["width"] = width
-        img_info["raw_img"] = img
-        ratio = min(self.test_size[0] / img.shape[0], self.test_size[1] / img.shape[1])
-        img_info["ratio"] = ratio
-
-        img, _ = self.preproc(img, None, self.test_size)
-        img = torch.from_numpy(img).unsqueeze(0)
-        img = img.float()
-        if self.device == "gpu":
-            img = img.cuda()
-            if self.fp16:
-                img = img.half()
-        with torch.no_grad():
-            t0 = time.time()
-            outputs = self.model(img)
-            t1 = time.time()
-            
-            if self.decoder is not None: 
-                outputs = self.decoder(outputs, dtype=outputs.type())
-            outputs = postprocess(
-                    outputs, self.num_classes, self.confthre, self.nmsthre, class_agnostic=True)
-        return outputs, img_info
-
-    def visual(self, output, img_info, cls_conf=0.35):
-        ratio = img_info["ratio"]
-        img = img_info["raw_img"]
-        if output is None:
-            return img
-        output = output.cpu()
-        bboxes = output[:, 0:4]
-
-        # preprocessing: resize
-        bboxes /= ratio
-        cls = output[:,6]
-        scores = output[:,4] * output[:,5]
-
-        vis_res = vis(img, bboxes, scores, cls, cls_conf, self.cls_names)
-        return vis_res
-
 
 def imageflow(predictor, vis_folder, current_time, args):
     
@@ -152,7 +75,7 @@ def imageflow(predictor, vis_folder, current_time, args):
         t_loop_start = time.time()
         
         ret_val, frame = cap.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         if ret_val:
             outputs, img_info = predictor.inference(frame)
