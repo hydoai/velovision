a13b209933eb9a1c907cda7667f8c05b002523e9
Jason Sohn <tensorturtle@gmail.com>
2021-10-27 02:02:32 +0900

    try sudo from python

--- sensing_interface/cameras.py
+++ sensing_interface/cameras.py
@@ -13,23 +13,38 @@ Use example:
 
 import cv2
 import os
+import subprocess
 
-def create_cv_cap_piv2()
+class CameraInterface:
+    def __init__(self, sudo_password):
+        self.sudo_password = sudo_password
+        
+        self.num_virtual_devices = 4
+        self.max_buffers = 2
+        modprobe_command = f'sudo modprobe v4l2loopback devices={self.num_virtual_devices} max_buffers=2{self.max_buffers}'
+        os.system('echo %s|sudo -S %s' % (self.sudo_password, modprobe_command))
 
-def create_cv_cap_imx291()
+        
 
-def clone_stream()
+def create_cv_cap_piv2():
+    pass
 
-def record_stream_piv2()
+def create_cv_cap_imx291():
+    pass
 
-def record_stream_imx291()
+def clone_stream():
+    pass
+
+def record_stream_piv2():
+    pass
+
+def record_stream_imx291():
+    pass
 
 
 if __name__ == '__main__':
     # testing
     print("Make sure that Raspberry Pi V2 camera is connected to CSI")
     print("Make sure that IMX291 USB camera is plugged in")
-
-    cap0 = create_cv_cap_pi()
-
-
+    
+    camint = CameraInterface()
