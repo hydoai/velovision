d8b4dd7c269e5169b0ef4380ea14911cff04f422
tensorturtle <tensorturtle@gmail.com>
2021-12-16 21:48:50 +0900

    extend wall time tolerance (github runner is damn slow)

--- computer_vision/test_vision.py
+++ computer_vision/test_vision.py
@@ -42,7 +42,7 @@ def test_core_cpu():
     tolerance = {
         'num_front_warnings': (0,0), # true value minus first value, true value minus second value are all allowed
         'num_rear_warnings': (0,0),
-        'wall_time_elapsed': (5,10)
+        'wall_time_elapsed': (5,60)
     }
     reality = core(exp,args)
     print(reality)
