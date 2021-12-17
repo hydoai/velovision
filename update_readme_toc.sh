af3a0fd3b11645cc23059fe9cc1983c29bd82758
tensorturtle <tensorturtle@gmail.com>
2021-12-17 14:27:07 +0900

    update

--- update_readme_toc.sh
+++ update_readme_toc.sh
@@ -1,2 +1,5 @@
+# shortcut script to update table of contents in README.md
+
+# if first time, uncomment below install line:
 #sudo npm install -g doctoc
 doctoc --title '# Table of Contents' --maxlevel 1 README.md
